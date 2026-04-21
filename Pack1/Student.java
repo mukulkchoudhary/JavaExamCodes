afe828612735b799040d7d23dbb6d2f1ee15baee
Mukul Kumar <mukul.kumar.it28@heritageit.edu.in>
2026-04-21 12:52:26 +0530

    Update Student.java

--- Pack1/Student.java
+++ Pack1/Student.java
@@ -7,38 +7,38 @@ public class Student extends Hostel implements Department {
     private int regNo;
     private String electiveSubject;
     private double avgMarks;
-    public String depName;
-    public String depHead;
+    private String depName;
+    private String depHead;
     
-    public void getData() {
+    public void getStudentData() {
         Scanner sc = new Scanner(System.in);
-        
         System.out.print("Enter student name: ");
         studentName = sc.nextLine();
-        
         System.out.print("Enter registration number: ");
         regNo = sc.nextInt();
         sc.nextLine();
-        
         System.out.print("Enter elective subject: ");
         electiveSubject = sc.nextLine();
-        
         System.out.print("Enter average marks: ");
         avgMarks = sc.nextDouble();
-        sc.nextLine();
-        
-        System.out.print("Enter department name: ");
-        depName = sc.nextLine();
-        
-        System.out.print("Enter department head: ");
-        depHead = sc.nextLine();
     }
     
-    public void printData() {
+    public void printStudentData() {
         System.out.println("Student Name: " + studentName);
         System.out.println("Registration Number: " + regNo);
         System.out.println("Elective Subject: " + electiveSubject);
         System.out.println("Average Marks: " + avgMarks);
+    }
+    
+    public void getDepartmentData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter department name: ");
+        depName = sc.nextLine();
+        System.out.print("Enter department head: ");
+        depHead = sc.nextLine();
+    }
+    
+    public void printDepartmentData() {
         System.out.println("Department Name: " + depName);
         System.out.println("Department Head: " + depHead);
     }
