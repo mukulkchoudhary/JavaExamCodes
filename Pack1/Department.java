413c57281555f4dd882ba136020bab070409b30c
Mukul Kumar <mukul.kumar.it28@heritageit.edu.in>
2026-04-21 12:42:58 +0530

    Update Department.java

--- Pack1/Department.java
+++ Pack1/Department.java
@@ -1,9 +1,6 @@
 package Pack1;
 
 public interface Department {
-    String depName = "Computer Science";
-    String depHead = "Dr. Sharma";
-    
-    void getData();
-    void printData();
+    void getDepartmentData();
+    void printDepartmentData();
 }
