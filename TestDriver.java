c5085f0503f3c7745521d054a305f1812db55199
Mukul Kumar <mukul.kumar.it28@heritageit.edu.in>
2026-04-21 12:52:05 +0530

    Update TestDriver.java

--- TestDriver.java
+++ TestDriver.java
@@ -1,20 +1,26 @@
 import Pack1.Student;
-import java.util.Scanner;
 
 public class TestDriver {
     public static void main(String[] args) {
         Student s = new Student();
         
         System.out.println("===== ENTER HOSTEL DETAILS =====");
-        s.getData();
+        s.getHostelData();
         
-        System.out.println("\n===== ENTER STUDENT AND DEPARTMENT DETAILS =====");
-        s.getData();
+        System.out.println("\n===== ENTER STUDENT DETAILS =====");
+        s.getStudentData();
         
-        System.out.println("\n===== HOSTEL INFORMATION =====");
-        s.printData();
+        System.out.println("\n===== ENTER DEPARTMENT DETAILS =====");
+        s.getDepartmentData();
         
-        System.out.println("\n===== STUDENT AND DEPARTMENT INFORMATION =====");
-        s.printData();
+        System.out.println("\n\n===== COMPLETE RECORD =====");
+        System.out.println("\n--- HOSTEL INFORMATION ---");
+        s.printHostelData();
+        
+        System.out.println("\n--- STUDENT INFORMATION ---");
+        s.printStudentData();
+        
+        System.out.println("\n--- DEPARTMENT INFORMATION ---");
+        s.printDepartmentData();
     }
 }
