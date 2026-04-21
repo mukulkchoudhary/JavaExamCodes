2d0b7577cc460ce615d2c7362506dbc6e8f66bc5
Mukul Kumar <mukul.kumar.it28@heritageit.edu.in>
2026-04-21 12:00:57 +0530

    Add abstract class ThreeDObject with dimensions

---
+++ ThreeDObject.java
@@ -0,0 +1,8 @@
+abstract class ThreeDObject {
+    protected double dim1;
+    protected double dim2;
+    protected double dim3;
+    
+    abstract double wholeSurfaceArea();
+    abstract double volume();
+}
