c1196312f3f0bf8f8471ede55bb752d6dfadc134
Mukul Kumar <mukul.kumar.it28@heritageit.edu.in>
2026-04-21 12:06:12 +0530

    Add Hostel class with data input and output methods

---
+++ Pack1/Hostel.java
@@ -0,0 +1,25 @@
+package Pack1;
+
+import java.util.Scanner;
+
+public class Hostel {
+    protected String hostelName;
+    protected String hostelLocation;
+    protected int noOfRooms;
+    
+    public void getHostelData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter hostel name: ");
+        hostelName = sc.nextLine();
+        System.out.print("Enter hostel location: ");
+        hostelLocation = sc.nextLine();
+        System.out.print("Enter number of rooms: ");
+        noOfRooms = sc.nextInt();
+    }
+    
+    public void printHostelData() {
+        System.out.println("Hostel Name: " + hostelName);
+        System.out.println("Hostel Location: " + hostelLocation);
+        System.out.println("Number of Rooms: " + noOfRooms);
+    }
+}
