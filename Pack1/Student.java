7bbe77c81ed7cec4a79150c7999cd2a923940631
Mukul Kumar <mukul.kumar.it28@heritageit.edu.in>
2026-04-21 12:51:19 +0530

    Update Student.java

--- Pack1/Student.java
+++ Pack1/Student.java
@@ -7,63 +7,39 @@ public class Student extends Hostel implements Department {
     private int regNo;
     private String electiveSubject;
     private double avgMarks;
+    public String depName;
+    public String depHead;
     
-    // Department attributes stored here
-    private String depName;
-    private String depHead;
-    
-    // Implementing Department interface methods
-    public void getDepartmentData() {
+    public void getData() {
         Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter student name: ");
+        studentName = sc.nextLine();
+        
+        System.out.print("Enter registration number: ");
+        regNo = sc.nextInt();
+        sc.nextLine();
+        
+        System.out.print("Enter elective subject: ");
+        electiveSubject = sc.nextLine();
+        
+        System.out.print("Enter average marks: ");
+        avgMarks = sc.nextDouble();
+        sc.nextLine();
+        
         System.out.print("Enter department name: ");
         depName = sc.nextLine();
+        
         System.out.print("Enter department head: ");
         depHead = sc.nextLine();
     }
     
-    public void printDepartmentData() {
-        System.out.println("Department Name: " + depName);
-        System.out.println("Department Head: " + depHead);
-    }
-    
-    // Student's own methods
-    public void getStudentData() {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter student name: ");
-        studentName = sc.nextLine();
-        System.out.print("Enter registration number: ");
-        regNo = sc.nextInt();
-        sc.nextLine();
-        System.out.print("Enter elective subject: ");
-        electiveSubject = sc.nextLine();
-        System.out.print("Enter average marks: ");
-        avgMarks = sc.nextDouble();
-    }
-    
-    public void printStudentData() {
+    public void printData() {
         System.out.println("Student Name: " + studentName);
         System.out.println("Registration Number: " + regNo);
         System.out.println("Elective Subject: " + electiveSubject);
         System.out.println("Average Marks: " + avgMarks);
-    }
-    
-    // Combined method to get ALL data
-    public void getAllData() {
-        System.out.println("\n--- Enter Hostel Details ---");
-        getHostelData();
-        System.out.println("\n--- Enter Student Details ---");
-        getStudentData();
-        System.out.println("\n--- Enter Department Details ---");
-        getDepartmentData();
-    }
-    
-    // Combined method to print ALL data
-    public void printAllData() {
-        System.out.println("\n===== HOSTEL INFORMATION =====");
-        printHostelData();
-        System.out.println("\n===== STUDENT INFORMATION =====");
-        printStudentData();
-        System.out.println("\n===== DEPARTMENT INFORMATION =====");
-        printDepartmentData();
+        System.out.println("Department Name: " + depName);
+        System.out.println("Department Head: " + depHead);
     }
 }
